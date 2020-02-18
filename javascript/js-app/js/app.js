23217eabfdb10ca4a3e78d4975e54c04ddda6a37
petesalcedo <peter.salcedo.diaz@gmail.com>
2020-02-18 16:45:46 -0600

    Adding card functionality

--- javascript/js-app/js/app.js
+++ javascript/js-app/js/app.js
@@ -1,5 +1,8 @@
 import {Users} from './users';
 
+let name = document.getElementById("name")
+let username = document.getElementById("username")
+let email = document.getElementById("email")
 
 class App{
    constructor(){
@@ -20,12 +23,31 @@ class App{
 		var menu = e.target.getAttribute('menu');
 	document.querySelectorAll('.routes>div').forEach(item =>item.classList.remove('active'))
 	document.querySelector(`.routes>div.${menu}`).classList.add('active');
-	
 	}
 getUsers(){
-	this.users.getUsers().then(data => console.log(data));
+	this.users.getUsers().then(data => data.forEach(this.printToDom))
 }
+printToDom(data){
+	let card = 
+`
+<div class="card">
+	<dl>
+	<dt>Name</dt> <dd>${data.name}</dd>
+	<dt>Email</dt><dd>${data.email}</dd>
+	</dl>
+	<h3> Adress</h3>
+	<dl>
+	  <dt>Street</dt><dd>${data.street}</dd>
+	 <dt>City</dt><dd>${data.city}</dd>
+	</dl>
+	</div>`
+	let div = document.createElement('div');
+	div.innerHTML=card
+	document.querySelector('.users').appendChild(div);
 }
-const app = new App();
 
+
+}
+
+const app = new App();
 
