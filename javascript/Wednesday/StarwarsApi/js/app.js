0dbe89f0381571ddd2bb4cbc57181c089d34a59a
PeteSalcedo <peter.salcedo.diaz@gmail.com>
2020-02-20 08:09:32 -0600

    Adding the dom

--- javascript/Wednesday/StarwarsApi/js/app.js
+++ javascript/Wednesday/StarwarsApi/js/app.js
@@ -1,21 +1,42 @@
-import {Characters} from './names'
+import {
+  Characters
+} from './names'
 
-
-class StarWars{
-  constructor(){
+class StarWars {
+  constructor() {
     this.characters = new Characters
     this.getPeople()
+    this.getInfoForActors()
 
+  }
+  getPeople() {
+    this.characters.getUsers().then(data => data.results.forEach(this.printToDOM))
+  }
+  printToDOM(data) {
+    let card = `
+<div class="card">
+<a href id="name" class="name">${data.name}</a>
+</div>
+`
+    let div = document.createElement('div');
+    div.innerHTML = card
+    document.querySelector('.vertical-menu').appendChild(div);
+    document.getElementsByClassName('.actors').innerHTML = data.name
+  }
+getInfoForActors(){
+  this.characters.getUsers().then(data => data.results.forEach(item => console.log(item.name)))
 }
-getPeople(){
-  this.characters.getUsers().then(data => data.results.forEach(item => console.log(item)))
+printActorsInfo(data){
+  let infoCard = `
+  <div class="infoCard">
+  <a href id="name" class="name">${data.name}</a>
+  </div>
+  `
 }
-printToDOM(data){
-  document.getElementsById('name').innerHTML = 'peter'
 
-
-}
 }
 
 
 const peter = new StarWars
+
+
